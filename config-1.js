4b6af0dd23d7ec449ab44933bdfb0388a6a9893d
lol109435 <78747696+lol109435@users.noreply.github.com>
2022-05-18 15:42:26 +0200

    -
    -

---
+++ config-1.js
@@ -0,0 +1,8 @@
+
+window._mtm = window._mtm || {};
+window._mtm.projectId = "prj:d1fa7c47-63da-4be3-b4ec-a227bb98b211";
+window._mtm.configAutoblocking =
+{
+    "rules": [],
+    "autoblockingEnabled": true
+}
